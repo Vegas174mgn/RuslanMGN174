294baebb3f008162a1f7fd77544ccfa819fbc10d
Knyazev Ruslan <rusland174@gmail.com>
2023-05-16 21:45:33 +0500

    Singleton for connection realized

---
+++ app/php/db/Connection.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\db;
+
+use PDO;
+
+class Connection
+{
+    /**
+     * PDO объект.
+     *
+     * @var PDO
+     */
+    private PDO $pdo;
+
+    /**
+     * Конфигурационные данные БД.
+     *
+     * @var array
+     */
+    protected array $config = [];
+
+    private static ?Connection $instance = null;
+
+    public function __construct()
+    {
+        $db_conf = require "configDB.php";
+
+        $this->config = $db_conf;
+        $this->connect();
+    }
+
+    private function connect(): void
+    {
+        $dsn = "mysql:host={$this->config["host"]};dbname={$this->config['dbname']};charset={$this->config["charset"]}";
+        $options = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->config["username"], $this->config["password"], $options);
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    private function __clone() {}
+
+    public static function getInstance(): ?Connection
+    {
+        if (null === self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize singleton");
+    }
+
+    public function getPDO (): PDO
+    {
+        return $this->pdo;
+    }
+}
---
+++ app/php/db/DbHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\db;
+
+use PDO;
+use PDOStatement;
+
+require_once "Connection.php";
+
+class DbHandler
+{
+    /**
+     * Объект PDOStatement.
+     * (набор всех полученных из базы данных строк)
+     *
+     * @var PDOStatement
+     */
+    private PDOStatement $statement;
+
+    /**
+     * Подключение к БД
+     *
+     * @var Connection
+     */
+    private Connection $connect;
+
+    /**
+     * Подключены ли к БД.
+     *
+     * @var bool
+     */
+    private bool $isConnected = false;
+
+    private function init(string $query): void
+    {
+        if (!$this->isConnected) {
+            $this->connect = Connection::getInstance();
+            $this->isConnected = true;
+        }
+
+        try {
+            $this->statement = $this->connect->getPDO()->prepare($query);
+            $this->statement->execute();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function insertUser($name, $email, $pass): void
+    {
+        $sql = "INSERT INTO `users` (`name`, `email`, `pass`) VALUES ('$name', LOWER('$email'), '$pass')";
+        $this->init($sql);
+    }
+
+    public function getUserByEmail($email, $mode = PDO::FETCH_ASSOC): array | bool
+    {
+        $sql = "SELECT * FROM `users` WHERE LOWER(email = '$email')";
+        $this->init($sql);
+
+        return $this->statement->fetch($mode);
+    }
+
+    public function updateUserPassword($userEmail, $password): void
+    {
+        $sql = "UPDATE `users` SET `pass`='$password' WHERE email = '$userEmail'";
+        $this->init($sql);
+
+    }
+
+    public function query(string $query, $mode = PDO::FETCH_ASSOC)
+    {
+        $query = trim(str_replace("\r", "", $query));
+
+        $this->init($query);
+
+        $rawStatement = explode(" ", preg_replace("/\s+|\t+|\n+/", " ", $query));
+
+        $statement = strtolower($rawStatement[0]);
+
+        if ($statement === "select" || $statement === "show") {
+            return $this->statement->fetchAll($mode);
+        } elseif ($statement === "insert" || $statement === "update" || $statement === "delete") {
+            return $this->statement->rowCount();
+        } else {
+            return null;
+        }
+    }
+
+}
